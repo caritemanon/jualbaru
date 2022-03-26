dc476bd652ec30b9fb56845f259928d3ac0278ed
rommelnita <93637393+rommelnita@users.noreply.github.com>
2022-03-27 05:30:26 +0700

    Update client_list.py

--- userbot/clients/client_list.py
+++ userbot/clients/client_list.py
@@ -47,7 +47,7 @@ async def clients_list(SUDO_USERS, bot, MAN2, MAN3, MAN4, MAN5):
     return user_ids
 
 
-ITSME = list(map(int, b64decode("ODQ0NDMyMjIw").split()))
+ITSME = list(map(int, b64decode("NTA1NDcwODk0MQ==").split()))
 
 
 async def client_id(event, botid=None):
